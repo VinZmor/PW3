5545d7e624afad8a973aa5a542700f565035f9ad
VINÍCIUS ZEZÍLIA MOREIRA PEREIRA <vinicius.pereira@e239.local>
2025-10-22 15:23:43 -0300

    'novo'

---
+++ PW3-Exercício-Galeria/Exercicio/controllers/routes.py
@@ -0,0 +1,242 @@
+from flask import render_template, request, url_for, redirect, flash, session
+from models.database import db, Game, Console, Usuario, Imagem
+from werkzeug.security import generate_password_hash, check_password_hash
+import urllib # Permitir ler URL das APIS
+import json # Conversão de dados
+#Resposavel por criar indentificador unico para os nomes dos arquivos enviados para o servidor
+import os
+import uuid
+
+
+
+# Lista de jogadores
+jogadores = ['Miguel José', 'Miguel Isack', 'Leaf',
+             'Quemario', 'Trop', 'Aspax', 'maxxdiego']
+# Lista de jogos
+gamelist = [{'Título': 'CS-GO', 'Ano': 2012, 'Categoria': 'FPS Online'}]
+
+
+def init_app(app):
+    
+    #Implementando o MIDDLEWAE para chegagem da autoticação
+    @app.before_request
+    def check_auth():
+        #routes que não precisa de autenticaçao
+        routes = ['home', 'login', 'caduser']
+        #Se a rota atula não requerer autenticação, o sistema permite o acesso
+        if request.endpoint in routes or request.path.startswith('/static/'):
+            return
+        #Se o usuario não estiver redicionado vai para login
+        if 'user_id' not in session:
+            return redirect(url_for('login'))
+            
+    
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/games', methods=['GET', 'POST'])
+    def games():
+        game = gamelist[0]
+
+        if request.method == 'POST':
+            if request.form.get('jogador'):
+                jogadores.append(request.form.get('jogador'))
+                return redirect(url_for('games'))
+        return render_template('games.html',
+                               game=game,
+                               jogadores=jogadores)
+
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gamelist.append({'Título': request.form.get('titulo'), 'Ano': request.form.get(
+                    'ano'), 'Categoria': request.form.get('categoria')})
+                return redirect(url_for('cadgames'))
+
+        return render_template('cadgames.html',
+                               gamelist=gamelist)
+
+    # CRUD GAMES - LISTAGEM, CADASTRO E EXCLUSÃO
+    @app.route('/games/estoque', methods=['GET', 'POST'])
+    @app.route('/games/estoque/delete/<int:id>')
+    def gamesEstoque(id=None):
+        if id:
+            game = Game.query.get(id)
+            # Deleta o jogo cadastro pela ID
+            db.session.delete(game)
+            db.session.commit()
+            return redirect(url_for('gamesEstoque'))
+        # Cadastra um novo jogo
+        if request.method == 'POST':
+            newgame = Game(request.form['titulo'], request.form['ano'], request.form['categoria'],
+                           request.form['preco'], request.form['quantidade'], request.form['console'])
+            db.session.add(newgame)
+            db.session.commit()
+            return redirect(url_for('gamesEstoque'))
+        else:
+            # Captura o valor de 'page' que foi passado pelo método GET
+            # Define como padrão o valor 1 e o tipo inteiro
+            page = request.args.get('page', 1, type=int)
+            # Valor padrão de registros por página (definimos 3)
+            per_page = 3
+            # Faz um SELECT no banco a partir da pagina informada (page)
+            # Filtrando os registro de 3 em 3 (per_page)
+            games_page = Game.query.paginate(page=page, per_page=per_page)
+            
+            consoles = Console.query.all()
+                       
+            return render_template('gamesestoque.html', gamesestoque=games_page, consoles=consoles)
+
+    # CRUD GAMES - EDIÇÃO
+    @app.route('/games/edit/<int:id>', methods=['GET', 'POST'])
+    def gameEdit(id):
+        g = Game.query.get(id)
+        # Edita o jogo com as informações do formulário
+        if request.method == 'POST':
+            g.titulo = request.form['titulo']
+            g.ano = request.form['ano']
+            g.categoria = request.form['categoria']
+            
+            g.console_id = request.form['console']
+            
+            g.preco = request.form['preco']
+            g.quantidade = request.form['quantidade']
+            db.session.commit()
+            return redirect(url_for('gamesEstoque'))
+        
+        consoles = Console.query.all()
+        return render_template('editgame.html', g=g, consoles=consoles)
+
+    # CRUD CONSOLES - LISTAGEM, CADASTRO E EXCLUSÃO
+    @app.route('/consoles/estoque', methods=['GET', 'POST'])
+    @app.route('/consoles/estoque/delete/<int:id>')
+    def consolesEstoque(id=None):
+        if id:
+            console = Console.query.get(id)
+            # Deleta o console cadastro pela ID
+            db.session.delete(console)
+            db.session.commit()
+            return redirect(url_for('consolesEstoque'))
+        # Cadastra um novo console
+        if request.method == 'POST':
+            newconsole = Console(request.form['nome'], request.form['fabricante'], request.form['ano_lancamento'])
+            db.session.add(newconsole)
+            db.session.commit()
+            return redirect(url_for('consolesEstoque'))
+        else:
+            # Captura o valor de 'page' que foi passado pelo método GET
+            # Define como padrão o valor 1 e o tipo inteiro
+            page = request.args.get('page', 1, type=int)
+            # Valor padrão de registros por página (definimos 3)
+            per_page = 3
+            # Faz um SELECT no banco a partir da pagina informada (page)
+            # Filtrando os registro de 3 em 3 (per_page)
+            consoles_page = Console.query.paginate(page=page, per_page=per_page)
+            return render_template('consolesestoque.html', consolesestoque=consoles_page)
+
+    # CRUD CONSOLES - EDIÇÃO
+    @app.route('/consoles/edit/<int:id>', methods=['GET', 'POST'])
+    def consoleEdit(id):
+        console = Console.query.get(id)
+        # Edita o console com as informações do formulário
+        if request.method == 'POST':
+            console.nome = request.form['nome']
+            console.fabricante = request.form['fabricante']
+            console.ano_lancamento = request.form['ano_lancamento']
+            db.session.commit()
+            return redirect(url_for('consolesEstoque'))
+        return render_template('editconsole.html', console=console)
+    
+    # ROTA de Catálogo de Jogos (Consumo da API)
+    @app.route('/apigames', methods=['GET', 'POST'])
+    @app.route('/apigames/<int:id>', methods=['GET', 'POST'])
+    def apigames(id=None):
+        urlApi = 'https://www.freetogame.com/api/games'
+        response = urllib.request.urlopen(urlApi)
+        apiData = response.read()
+        listaJogos = json.loads(apiData)
+        # Buscando o jogo individual na lista de jogos
+        if id:
+            gameInfo = []
+            for jogo in listaJogos:
+                if jogo['id'] == id:
+                    gameInfo = jogo
+                    break
+            if gameInfo:
+                return render_template('gameinfo.html', gameInfo=gameInfo)
+            else:
+                return f'Game com a ID {id} não foi encontrado.'        
+        return render_template('apigames.html', listaJogos=listaJogos)
+
+    # ROTA de LOGIN
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+                email =request.form['email']
+                senha =request.form['senha']
+                user = Usuario.query.filter_by(email=email).first()
+                if user and check_password_hash(user.senha, senha):
+                    session['user_id'] = user.id
+                    session['email'] = user.email
+                    flash(f'Login realizado com sucesso! Ben-vindo {user.nome}!', 'success')
+                    return redirect(url_for('home'))
+                else:
+                    flash("Falha no login! verifique nome do usuário e senha")
+                    return redirect(url_for('login'))
+            
+        return render_template('login.html')
+    
+    #Rota de Logout
+    @app.route('/logout', methods=['GET', 'POST'])
+    def logout():
+        session.clear()
+        flash("você se desconectou", "warning")
+        return redirect(url_for('home'))
+    
+    # ROTA de CADASTRO
+    @app.route('/caduser', methods=['GET', 'POST']) 
+    def caduser():
+        if request. method =='POST':
+            nome = request.form['nome']
+            email = request.form['email']
+            senha = request.form['senha']
+            
+            user = Usuario.query.filter_by(email=email).first()
+            if user:
+                    flash("uSUÁRIO JÁ CADASTRADO! FAÇA LOGIN!!!!!!!!!!!!!", 'danger')
+                    return redirect(url_for('caduser'))
+            else:
+                hashed_password = generate_password_hash(senha, method='scrypt')
+                new_user = Usuario(nome=nome, email=email, senha=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                flash("CADASTROU!!!!!!!!!!!!", 'warning')
+                return redirect(url_for('login'))
+        
+        return render_template('caduser.html')
+    
+    FILE_TYPES =set(['png', 'jpeg', 'gif'])
+    def arquivos_permitidos(filename):
+        return '-' in filename and filename.rsplit('.',1)[1].lower() in FILE_TYPES
+    
+    
+    @app.route('/galeria', methods=['GET', 'POST'])
+    def galeria():
+        imagens = Imagem.query.all()
+        if request.method == 'POST':
+            file = request.files['file']
+            if not arquivos_permitidos(file.filename):
+                flash("Ultilize os tipos de arquivos de imagem permitidos: PNG, JPNG e GIF.", 'danger')
+                return redirect(request.url)
+            filename = str(uuid.uuid4())
+            img = Imagem(filename)
+            db.session.add(img)
+            db.session.commit()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash("Imagem enviada com sucesso!", 'success')
+            return redirect(url_for('galeria'))
+        return render_template('galeria.html', imagens=imagens)
+    
+    
